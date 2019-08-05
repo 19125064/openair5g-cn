34143ba3ebabe908a390703a8514e877f2d2b114
wangyongshou <wangys2009bj@163.com>
2019-08-04 22:54:20 -0700

    modify initial code by doc

--- src/amf/N1N2ProtocolStack/AmfNgapLayer/ngap_amf_itti_messaging.h
+++ src/amf/N1N2ProtocolStack/AmfNgapLayer/ngap_amf_itti_messaging.h
@@ -46,21 +46,43 @@ static inline void ngap_amf_itti_amf_app_initial_ue_message(
   AMF_APP_INITIAL_UE_MESSAGE(message_p).ran_ue_ngap_id               = ran_ue_ngap_id;
   AMF_APP_INITIAL_UE_MESSAGE(message_p).amf_ue_ngap_id               = amf_ue_ngap_id;
   AMF_APP_INITIAL_UE_MESSAGE(message_p).nas                          = blk2bstr(nas_msg,nas_msg_length);
-  AMF_APP_INITIAL_UE_MESSAGE(message_p).as_cause                     = rrc_cause;      
   AMF_APP_INITIAL_UE_MESSAGE(message_p).tai                          = *tai;
   AMF_APP_INITIAL_UE_MESSAGE(message_p).cgi                          = *cgi;
-  AMF_APP_INITIAL_UE_MESSAGE(message_p).is_fiveG_s_tmsi_valid        = fiveG_s_tmsi == NULL? false: true;
-  AMF_APP_INITIAL_UE_MESSAGE(message_p).is_amf_set_id_valid          = amfSetID     == NULL? false: true;
-  AMF_APP_INITIAL_UE_MESSAGE(message_p).is_allowed_nssai_valid       = allowedNSSAI == NULL? false: true;
-  
-  AMF_APP_INITIAL_UE_MESSAGE(message_p).fiveG_s_tmsi                 = *fiveG_s_tmsi;
-  AMF_APP_INITIAL_UE_MESSAGE(message_p).amf_set_id                   = *amfSetID;
-  AMF_APP_INITIAL_UE_MESSAGE(message_p).allowedNSSAI                 = *allowedNSSAI;
-
+  AMF_APP_INITIAL_UE_MESSAGE(message_p).as_cause                     = rrc_cause; 
   AMF_APP_INITIAL_UE_MESSAGE(message_p).transparent.ran_ue_ngap_id   = ran_ue_ngap_id;
   AMF_APP_INITIAL_UE_MESSAGE(message_p).transparent.amf_ue_ngap_id   = amf_ue_ngap_id;
   AMF_APP_INITIAL_UE_MESSAGE(message_p).transparent.nr_cgi           = *cgi;
+  
+  if(!fiveG_s_tmsi)
+  {
+      AMF_APP_INITIAL_UE_MESSAGE(message_p).is_fiveG_s_tmsi_valid    = false;
+  }
+  else
+  {
+      AMF_APP_INITIAL_UE_MESSAGE(message_p).is_fiveG_s_tmsi_valid    = true;
+	  AMF_APP_INITIAL_UE_MESSAGE(message_p).fiveG_s_tmsi             = *fiveG_s_tmsi;
+  }
 
+  if(!amfSetID)
+  {
+      AMF_APP_INITIAL_UE_MESSAGE(message_p).is_amf_set_id_valid      = false;
+  }
+  else
+  {
+      AMF_APP_INITIAL_UE_MESSAGE(message_p).is_amf_set_id_valid      = true;
+	  AMF_APP_INITIAL_UE_MESSAGE(message_p).amf_set_id               = *amfSetID;
+  }
+  
+  if(!allowedNSSAI)
+  {
+      AMF_APP_INITIAL_UE_MESSAGE(message_p).is_allowed_nssai_valid   = false;
+  }
+  else
+  {
+      AMF_APP_INITIAL_UE_MESSAGE(message_p).is_allowed_nssai_valid   = true;
+	  AMF_APP_INITIAL_UE_MESSAGE(message_p).allowedNSSAI             = *allowedNSSAI;
+  }
+      
   itti_send_msg_to_task(TASK_AMF_APP, INSTANCE_DEFAULT, message_p);
   OAILOG_FUNC_RETURN (LOG_NGAP,0);
 }
